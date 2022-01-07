17aa512c8057d5a7159e18ed4d86d6a9791d13aa
thomgonzalez <thom.sgonzalez@gmail.com>
2022-01-07 10:45:07 -0600

    Fix, filters.

--- shop/api/views.py
+++ shop/api/views.py
@@ -1,7 +1,9 @@
-﻿from rest_framework import viewsets
+﻿from django.db.models.query import QuerySet
+from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import permission_classes
 
 from shop.models import Order, OrderDetail, Delivery
 
@@ -9,6 +11,28 @@ from .serializers import OrderSerializer, OrderFormSerializer, OrderDetailSerial
 from .filters import OrderFilter
 
 
+def get_data(queryset, **kwargs):
+	data = []
+	if kwargs.get('is_urgent') and kwargs.get('client_type') \
+		and kwargs.get('distribution') and kwargs.get('is_assortment'):
+
+		if kwargs.get('is_urgent') == '1' and kwargs.get('client_type') == '04'  \
+			and kwargs.get('distribution') == 'all' and kwargs.get('is_assortment') == '0':
+
+			orders = queryset.filter(
+				is_urgent=int(kwargs.get('is_urgent')), 
+				customer__client_type=kwargs.get('client_type'),
+				company__isnull=True, 
+				branch__isnull=True
+			)
+			pks = list(orders.values_list('pk', flat=True))
+			data = OrderDetail.objects.filter(pk__in=pks, assortment_date__isnull=True)
+	else:
+		pks = list(queryset.values_list('pk', flat=True))
+		data = OrderDetail.objects.filter(pk__in=pks)
+	return data
+
+@permission_classes((AllowAny,))
 class OrderViewSet(viewsets.GenericViewSet):
 	queryset = Order.objects.all()
 	serializer_class = OrderDetailSerializer
@@ -16,26 +40,16 @@ class OrderViewSet(viewsets.GenericViewSet):
 	permission_classes = (IsAuthenticated,)
 
 	def list(self, request):
-		is_urgent = self.request.query_params.get('is_urgent', None)
-		client_type = self.request.query_params.get('client_type', None)
-		distribution = self.request.query_params.get('distribution', None)
-		is_assortment = self.request.query_params.get('is_assortment', None)
-		print(is_urgent, client_type, distribution, is_assortment)
-		
-		orders = self.filter_queryset(self.get_queryset())
+		kwargs = {
+			'is_urgent': self.request.query_params.get('is_urgent', None),
+			'client_type': self.request.query_params.get('client_type', None),
+			'distribution': self.request.query_params.get('distribution', None),
+			'is_assortment': self.request.query_params.get('is_assortment', None)
+		}
 
-		if is_urgent == '1' and client_type == '04' and distribution == 'all' and is_assortment == '0':
-			orders = orders.filter(
-				is_urgent=int(is_urgent), 
-				customer__client_type=client_type,
-				company__isnull=True, 
-				branch__isnull=True
-			)
-			pks = list(orders.values_list('pk', flat=True))
-			queryset = OrderDetail.objects.filter(pk__in=pks, assortment_date__isnull=True)
-			print(queryset)
-
-		serializer = self.get_serializer(queryset, many=True)
+		queryset = self.filter_queryset(self.get_queryset())
+		data = get_data(queryset, **kwargs)
+		serializer = self.get_serializer(data, many=True)
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
 	def create(self, request):
